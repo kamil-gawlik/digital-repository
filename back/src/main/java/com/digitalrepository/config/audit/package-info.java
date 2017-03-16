02f9216ca447c7f880ef28827f212c1bd619fe6e
Yarlaxle <adaswudzinski94@gmail.com>
2017-03-16 15:35:21 +0100

    base application

---
+++ back/src/main/java/com/digitalrepository/config/audit/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Audit specific code.
+ */
+package com.digitalrepository.config.audit;
---
+++ gateway/src/main/java/com/digitalrepository/config/cassandra/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Cassandra specific configuration.
+ */
+package com.digitalrepository.config.cassandra;

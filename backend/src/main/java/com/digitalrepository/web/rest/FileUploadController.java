5a5d05f3795a3fd1bb73200e170839f8debfcd68
piotrmajcher94 <piotr.majcher94@gmail.com>
2017-04-01 12:05:04 +0200

    Changed FileUploadController - now receiving filesList, recordName, author and description in request body. Added MultipartToInputStreamConvert class which converts list of multipart files to byte array (not in use currently - made for tests purpose)

--- backend/src/main/java/com/digitalrepository/web/rest/FileUploadController.java
+++ backend/src/main/java/com/digitalrepository/web/rest/FileUploadController.java
@@ -1,6 +1,9 @@
 package com.digitalrepository.web.rest;
 
+import com.digitalrepository.web.rest.util.MultipartToInputStreamConverter;
+import com.mongodb.BasicDBList;
 import com.mongodb.BasicDBObject;
+import com.mongodb.Bytes;
 import com.mongodb.DBObject;
 import com.mongodb.gridfs.GridFSDBFile;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,15 +11,14 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.data.mongodb.gridfs.GridFsOperations;
+import org.springframework.data.mongodb.gridfs.GridFsTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.BufferedInputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.util.List;
 
 /**
@@ -29,36 +31,44 @@ import java.util.List;
 public class FileUploadController {
 
     @Autowired
-    private ApplicationContext applicationContext;
+    private GridFsTemplate gridFsTemplate;
 
     @RequestMapping(value = "", method = RequestMethod.POST, consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public ResponseEntity<String> handleFileUpload(@RequestBody MultipartFile file, String author, String description){
+    public ResponseEntity<String> handleFileUpload(@RequestBody List<MultipartFile> filesList, String recordName, String author, String description){
         try {
-            InputStream inputStream = new BufferedInputStream(file.getInputStream());
-            GridFsOperations gridFsOperations = (GridFsOperations)applicationContext.getBean("gridFsTemplate");
+
+            //For each file included in the record
+            for(MultipartFile file : filesList){
+
+                /**
+                 * Create metadate for the file
+                 */
+                DBObject metaData = new BasicDBObject();
+                metaData.put("record-name", recordName);
+                metaData.put("filename", file.getOriginalFilename());
+                metaData.put("author", author);
+                metaData.put("description", description);
+                metaData.put("content-type", file.getContentType());
 
 
-            DBObject metaData = new BasicDBObject();
-            metaData.put("author", author);
-            metaData.put("description", description);
-
-            gridFsOperations.store(inputStream, file.getOriginalFilename(), metaData);
-
+                /**
+                 * Save file to the MongoDB
+                 */
+                gridFsTemplate.store(file.getInputStream(), metaData);
+            }
         }catch (IOException ioE){
-            return new ResponseEntity<String>("Uploading failed", HttpStatus.CONFLICT);
+            return new ResponseEntity<String>("Uploading new record failed:\n" + ioE, HttpStatus.CONFLICT);
         }
-
-        return new ResponseEntity<String>("File uploaded", HttpStatus.OK);
+        return new ResponseEntity<String>("Record " + recordName + " uploaded", HttpStatus.OK);
     }
 
     @RequestMapping(value = "", method = RequestMethod.GET)
-    public ResponseEntity<String> handleFileFetching(@RequestParam("name")String name){
-        GridFsOperations gridFsOperations = (GridFsOperations)applicationContext.getBean("gridFsTemplate");
-        List<GridFSDBFile> result = gridFsOperations.find(
-            new Query().addCriteria(Criteria.where("filename").is(name)));
+    public ResponseEntity<String> handleFileFetching(@RequestParam("record-name")String recordName){
+
+        List<GridFSDBFile> result = gridFsTemplate.find(
+            new Query().addCriteria(Criteria.where("metadata.record-name").is(recordName)));
         String response = "";
         for(GridFSDBFile file : result){
-            response += file.getFilename();
             response += file.getMetaData();
         }
         return new ResponseEntity<String>(response, HttpStatus.OK);
